df5889dd063ee3b3878e7f77cb41bfd4d3034442
Iñigo Baez Gurruchaga <inigobaezgurruchaga@MacBook-Air-de-Inigo.local>
2024-02-14 13:51:10 +0100

    4.1-4.7

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+/* eslint-disable @stylistic/js/indent */
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+        'jest': true
+    },
+    'plugins': [
+        '@stylistic/js'
+    ],
+    'extends': 'eslint:recommended',
+    'overrides': [
+        {
+            'env': {
+                'node': true,
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        '@stylistic/js/indent': [
+            'error',
+            2
+        ],
+        '@stylistic/js/linebreak-style': [
+            'error',
+            'unix'
+        ],
+        '@stylistic/js/quotes': [
+            'error',
+            'single'
+        ],
+        '@stylistic/js/semi': [
+            'error',
+            'never'
+        ],
+    },
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 fullstackopen_4
+
+├── index.js
+├── app.js
+├── dist
+│   └── ...
+├── controllers
+│   └── notes.js
+├── models
+│   └── note.js
+├── package-lock.json
+├── package.json
+├── utils
+│   ├── config.js
+│   ├── logger.js
+│   └── middleware.js  
